e9dc8d4fbc87304e3f3def721d146c078059156a
T-double-U <trat.westerholt@gmail.com>
2018-03-12 22:35:44 +0200

    3d | +func & organized

--- First.py
+++ First.py
@@ -1,17 +1,28 @@
 import math, random
+
+
+def func(x):
+    if x == 4:
+        print(math.sqrt(x) + 4)
+    elif (x > -10) & (x <= 16):
+        print(x ** 2)
+    elif (x < -8):
+        print(x ** -1)
+    return
+
+
 handle = open("x.txt", "w")
-i=0
-j=input();
-while i<=j:
+i = 0
+j = input("Enter amount of x");
+j = int(j)
+while i <= j:
     x = random.uniform(-15, 20)
-    print(x)
-    handle.write(x)
+    func(x)
+    print("x: " + str(x))
+    handle.write(str(x))
+    i = i + 1
 handle.close()
-i=0;
-while i<=j:
-    if x==4:
-        print(math.sqrt(x)+4)
-    elif (x>-10) & (x<=16):
-        print(x**2)
-    elif (x<-8):
-        print(x**-1)
+
+
+
+
