0834081782cec3d81cdddfaa3efac868ab7ac917
T-double-U <trat.westerholt@gmail.com>
2018-03-12 23:05:58 +0200

    4th | +output +rearanged Функцию написал для простоты, однако в конце все пошло по пизде, и оно не выводит в файл, попробовал без функции и заново написал - та же хрень.. сейчас сижу, разбираюсь

--- First.py
+++ First.py
@@ -1,13 +1,13 @@
 import math, random
 
 
-def func(x):
+def func(x1):
     if x == 4:
-        print(math.sqrt(x) + 4)
+        return (math.sqrt(x) + 4)
     elif (x > -10) & (x <= 16):
-        print(x ** 2)
+        return (x ** 2)
     elif (x < -8):
-        print(x ** -1)
+        return (x ** -1)
     return
 
 
@@ -17,11 +17,33 @@ j = input("Enter amount of x");
 j = int(j)
 while i <= j:
     x = random.uniform(-15, 20)
-    func(x)
+    print(func(x))
     print("x: " + str(x))
-    handle.write(str(x))
+    handle.write(str(x)+"\n")
     i = i + 1
 handle.close()
+f = open('x.txt')
+for lines in f.readlines():
+    arr = lines
+y = open("y.txt", "w")
+while i<=j:
+
+    if int(arr[i]) == 4:
+        y.write(math.sqrt(int(arr[i])) + 4)
+        i=i+1
+        continue
+    elif (int(arr[i]) > -10) & (int(arr[i]) <= 16):
+        y.write(int(arr[i]) ** 2)
+        i=i+1
+        continue
+    elif (int(arr[i]) < -8):
+        y.write(int(arr[i]) ** -1)
+        i=i+1
+        print()
+        continue
+    print("Written")
+y.close()
+
 
 
 
