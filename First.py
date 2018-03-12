645c728c137a5b6e5d1db196c9d24a0de89164cf
T-double-U <trat.westerholt@gmail.com>
2018-03-12 22:20:48 +0200

    2 commit | +Read from file +Random numbers

--- First.py
+++ First.py
@@ -1,8 +1,17 @@
-import math
-x = float(input());
-if x==4:
-    print(math.sqrt(x)+4)
-elif (x>-10) & (x<=16):
-    print(x**2)
-elif (x<-8):
-    print(x**-1)
+import math, random
+handle = open("x.txt", "w")
+i=0
+j=input();
+while i<=j:
+    x = random.uniform(-15, 20)
+    print(x)
+    handle.write(x)
+handle.close()
+i=0;
+while i<=j:
+    if x==4:
+        print(math.sqrt(x)+4)
+    elif (x>-10) & (x<=16):
+        print(x**2)
+    elif (x<-8):
+        print(x**-1)
