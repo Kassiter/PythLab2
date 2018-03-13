ec30198ff1d89d947ee5b906f20babc2bcd53d9e
T-double-U <trat.westerholt@gmail.com>
2018-03-13 19:20:31 +0200

    v6.1 | Compacted code

--- First.py
+++ First.py
@@ -1,52 +1,55 @@
 import math, random
 
+handle = open("x.txt", "w")
+y = open("y.txt", "w")
+
+def toFile(name,k):
+    f = open(name, "w")
+    i1=0
+    while i1 <= k:
+        x = random.uniform(-15, 20)
+        f.write(str(x) + "\n")
+        i1 = i1 + 1
+    f.write("")
+    f.close()
+
+
+def fromFile(name):
+    output=[]
+    ff = open(name, "r+")
+    for lines in ff.readlines():
+        items = lines.strip("()\n").split(",")
+        output += items
+    ff.close()
+    return output
+
 
 def func(x1):
-    if x == 4:
-        return (math.sqrt(x) + 4)
-    elif (x > -10) & (x <= 16):
-        return (x ** 2)
-    elif (x < -8):
-        return (x ** -1)
+    if x1 == 4:
+        return (math.sqrt(float(x1)) + 4)
+    elif (float(x1) > -10) & (float(x1) <= 16):
+        return (float(x1) ** 2)
+    elif (float(x1) < -8):
+        return (float(x1) ** -1)
     return
 
 
-handle = open("x.txt", "w")
 i = 0
 j = input("Enter amount of x");
 j = int(j)
-while i <= j:
-    x = random.uniform(-15, 20)
-    print(func(x))
-    print("x: " + str(x))
-    handle.write(str(x)+"\n")
-    i = i + 1
-handle.write("")
-handle.close()
-f = open('x.txt')
+toFile("x.txt",j)
+
 arr=[]
 i=0
-for lines in f.readlines():
-    items = lines.strip("()\n").split(",")
-    arr += items
-y = open("y.txt", "w")
+arr=fromFile("x.txt")
 i=0
 yarr =arr
-print("arrlen: ")
-print(arr.__len__())
 while i<arr.__len__():
-    print("entered func")
-    if float(arr[i]) == 4:
-        yarr[i]=(str(math.sqrt(float(arr[i])) + 4))
-    elif (float(arr[i]) > -10) & (float(arr[i]) <= 16):
-        yarr[i] =(str(float(arr[i]) ** 2))
-    elif (float(arr[i]) < -8):
-        yarr[i] =(str(float(arr[i]) ** -1))
-    print("Written")
+    yarr[i]=func(arr[i])
     i=i+1
 i=0
 while i<yarr.__len__():
-    y.write(yarr[i]+"\n")
+    y.write(str(yarr[i])+"\n")
     i=i+1
 y.close()
 
